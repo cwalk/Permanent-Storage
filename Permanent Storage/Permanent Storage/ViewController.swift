cb42e5871d906251c7dcc897b1442847b75b6334
Clayton Walker <claytonwalkerucf@knights.ucf.edu>
2015-10-11 21:33:14 -0400

    Finished app, stores a username and an array into NSUserDefaults.standardUserDefaults()

--- Permanent Storage/Permanent Storage/ViewController.swift
+++ Permanent Storage/Permanent Storage/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSUserDefaults.standardUserDefaults().setObject("Clayton", forKey: "name")
+        
+        //Force unwrap in order to get rid of "Optional(username)" when it prints to the console
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! NSString
+        
+        print(userName)
+        
+        let myArray = [1, 2, 3, 4]
+        
+        NSUserDefaults.standardUserDefaults().setObject(myArray, forKey: "array")
+        
+        //as! forces it to be cast as an NSString or NSArray instead of AnyObject
+        let returnedArray = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        print(returnedArray)
+        
+        for x in returnedArray {
+            print(x)
+        }
     }
 
     override func didReceiveMemoryWarning() {
